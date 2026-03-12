5235d795eac17b928fe4ae5badbdea771a7e50d8
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-12 22:27:40 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
-# Developed by: LastPerson07 × cantarella
-# Telegram: @cantarellabots | @THEUPDATEDGUYS
+# Developed by: Arafta × anuj
+# Telegram: @arafta_hindi_dubbed_webseries | @anujedits76
 import os
 import asyncio
 import random
@@ -38,7 +38,7 @@ REACTIONS = [
 
 dev_text = "👨‍💻 Mind Behind This Bot:\n• @anujedits76\n• @arafta_hindi_dubbed_webseries"
 expected_dev_hash = "b9e63b7578bdec13f3cb3162fe5f5e93dccaba3bfd5c8ddacbb90ffdcdcce402"
-channels_text = "📢 Official Channels:\n• @arafta_hindi_dubbed_webseries\n• @arafta_hindi_dubbed_webseries\n\nStay updated for new features!"
+channels_text = "📢 Official Channels:\n• @arafta_hindi_dubbed_webseries\n• @anujedits76\n\nStay updated for new features!"
 expected_channels_hash = "e19212e571bd0f6626450dd790029d392c0748c554d4b386a0c0752f4148d37d"
 
 if (
